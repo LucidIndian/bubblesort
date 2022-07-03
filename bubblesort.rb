15edb0898fd202815329a92d5ad8fda1df02f839
Tygh Walters <tyghwalters@gmail.com>
2022-07-02 19:09:29 -0700

    refactor code to remove testing comments and unused logic

--- bubblesort.rb
+++ bubblesort.rb
@@ -2,16 +2,12 @@ def bubble_sort (messyArray)
   puts "Unsorted start array: #{messyArray}."
   maxComparisons = messyArray.length - 1 # -1 to match # of comparisons needed 1st round
   indexMax = maxComparisons-1; # -1 to set the max index range for sortedArray
-  until indexMax < 1 # we still want to run the program when i = 0, but stop when it's -1.  
+  until indexMax < 1
     sortedArray = (0..indexMax).map do |index|
-      # NOTE: If I don't change the array as I got through it, the comparisions will not be correct
       if messyArray[index] >= messyArray[index+1]
-        #puts "switch em" # add 1 the index of the first num
         messyArray.insert(index+1, messyArray.delete_at(index))
-      elsif messyArray[index] <= messyArray[index+1]
-        #puts "keep em" # don't move the order
       else
-        puts "error"
+        # do nothing! Keep them as is
       end
     end
     indexMax -= 1
@@ -20,3 +16,4 @@ def bubble_sort (messyArray)
 end 
 
 bubble_sort([4,3,78,2,0,2])
+# => [0,2,2,3,4,78]
