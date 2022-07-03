6fde90bc5fa44fa4239f25bc5515ae7cbc1d8b51
Tygh Walters <tyghwalters@gmail.com>
2022-07-02 18:49:44 -0700

    Method bubble_sort returns a sorted array from an unsorted array

--- bubblesort.rb
+++ bubblesort.rb
@@ -1,36 +1,22 @@
 def bubble_sort (messyArray)
-  # 1st "Pass": Compare 1st and 2nd array items
-  maxComparisons = messyArray.length - 1 # iterator (-1 to match indexes and 
-      puts "maxComparisons #{maxComparisons}" 
-
-    i = maxComparisons-1; # another -1 to stop comparison at 2nd last array item)
-    until i == 0
-
-      (0..i).map do |index|
-        
-        if messyArray[index] >= messyArray[index+1]
-          puts "switch em" # add 1 the index of the first num
-        elsif messyArray[index] <= messyArray[index+1]
-          puts "keep em" # don't move the order
-        else
-          puts "error"
-        end
+  puts "Unsorted start array: #{messyArray}."
+  maxComparisons = messyArray.length - 1 # -1 to match # of comparisons needed 1st round
+  indexMax = maxComparisons-1; # -1 to set the max index range for sortedArray
+  until indexMax < 1 # we still want to run the program when i = 0, but stop when it's -1.  
+    sortedArray = (0..indexMax).map do |index|
+      # NOTE: If I don't change the array as I got through it, the comparisions will not be correct
+      if messyArray[index] >= messyArray[index+1]
+        #puts "switch em" # add 1 the index of the first num
+        messyArray.insert(index+1, messyArray.delete_at(index))
+      elsif messyArray[index] <= messyArray[index+1]
+        #puts "keep em" # don't move the order
+      else
+        puts "error"
       end
-      i -= 1
     end
-  
- 
-  # If 1st is greater, swap, else keep order
-  # Shift comparison window one to the right (pos. 2 & 3), etc.
-    # repeat until end of array
-  # Start over, making 1 less comparision each pass (x passes = x #s sorted)
-  # Check each time(pass) if it's sorted (sorted == no swaps)...
-  #   or just run it the same # times as items in the collection
-  # Return the sorted array
-
-end
+    indexMax -= 1
+  end 
+  puts "Bubble-sorted array: #{sortedArray[0]}" # there's still a 2D array, but this yields the target result
+end 
 
 bubble_sort([4,3,78,2,0,2])
-
-# NOTES: 
-# can't use "sort" method  
